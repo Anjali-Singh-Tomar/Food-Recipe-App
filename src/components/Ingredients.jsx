81e19995627d8f0d0652f11caf26c79f14ecc2b0
Anjali-Singh-Tomar <tomaranjalisingh2812@gmail.com>
2026-02-17 18:42:38 +0530

    image modification

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -2,7 +2,12 @@ import styles from "./ingredients.module.css";
 export default function Ingredients({ ingredient }) {
   return (
     <div className={styles.outerContainer}>
-      <img src={ingredient.image} alt="" />
+      <img
+        src={
+          `https://spoonacular.com/cdn/ingredients_50x50/` + ingredient.image
+        }
+        alt=""
+      />
       <div className={styles.nameAndamount}>
         <span>{ingredient.name}</span>
         <span>{ingredient.amount}</span>
